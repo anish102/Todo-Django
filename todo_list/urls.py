02270e6a96e8d625f0edb78dccf834d633afd59b
anish102 <anishkarki695@gmail.com>
2022-11-04 12:33:08 +0545

    Added searching & user registration

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -2,17 +2,18 @@ from django.urls import path
 
 from todo_list.models import Task
 from . import views
-from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView,CustomLoginView
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('login/',CustomLoginView.as_view(),name='login'),
-    path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
 
-    path('',TaskList.as_view(),name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(),name = 'task'),
-    path('create_task/',TaskCreate.as_view(),name = 'task_create'),
-    path('update_task/<int:pk>/',TaskUpdate.as_view(),name = 'task_update'),
-    path('delete_task/<int:pk>/',DeleteView.as_view(),name = 'task_delete'),
-    
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create_task/', TaskCreate.as_view(), name='task_create'),
+    path('update_task/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
+    path('delete_task/<int:pk>/', DeleteView.as_view(), name='task_delete'),
+
 ]
